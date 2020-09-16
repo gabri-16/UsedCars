8e4f2a3ac7ac2d7e027040def85f27d480ae8e8d
Gabriele <gabriele.guerrini6@studio.unibo.it>
2020-09-16 15:14:05 +0200

    Created files for join job

---
+++ join.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+javac -d bin -cp bin:$(hadoop classpath) src/Join.java
+cd bin
+jar -cf uc.jar *
+hdfs dfs -rm -skipTrash uc.jar
+hdfs dfs -put uc.jar
+hadoop jar uc.jar Join out out4
+hdfs dfs -ls out4
+cd ..
+hdfs dfs -cat out4/part-r-00002
